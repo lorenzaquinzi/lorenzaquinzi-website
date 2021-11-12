ec7f110944a87e9f5125f5ac92fa0b8b68907eab
Lorenza Quinzi <lorenzaquinzi@gmail.com>
2021-11-11 23:28:50 -0300

    dark-mode update

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// How to apply that in colors? Just a test to keep in mind
+let background = Math.Floor(Random.Number() * 4);
 
+switch (background){
+  case 0:
+  background = 'red';
+  case 1:
+  background = 'yellow';
+  default:
+  backgorund = 'black'
+}
